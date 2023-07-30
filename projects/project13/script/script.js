69f897fc81a9cc30f6b3fba8e99cfb39ac4a1fd3
Alon <you@example.com>
2023-07-30 16:19:23 +0300

    added

--- projects/project13/script/script.js
+++ projects/project13/script/script.js
@@ -1,4 +1,4 @@
-var box = document.getElementById("calc-display");
+const box = document.getElementById("calc-display");
 
 // Function to display the pressed button value on the screen
 function toScreen(x) {
@@ -11,22 +11,22 @@ function toScreen(x) {
 
 function answer() {
   // Retrieve the expression from the display
-  x = box.value;
+  let x = box.value;
   x = eval(x);
   box.value = x;
 }
 
 function sqr() {
-  x = box.value;
+  let x = box.value;
   x = eval(x * x);
   box.value = x;
 }
 
 // Function to remove the last digit or operator entered into the display
 function backSpace() {
-  var num = box.value;
-  var len = num.length - 1;
-  var newNum = num.substring(0, len);
+  const num = box.value;
+  const len = num.length - 1;
+  const newNum = num.substring(0, len);
 
   // Display the new string (number or expression) on the screen
   box.value = newNum;
